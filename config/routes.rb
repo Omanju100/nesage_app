c6cfb35e2f422425f259ea25d4a60442142882fb
20220806 <a20220806@20220806noMacBook-Air.local>
2023-06-06 21:26:13 +0900

    finish?

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'settings#index'
   delete '/settings/:id', to: 'settings#destroy', as: 'delete_setting'
   patch '/settings', to: 'settings#update'
+  post '/login', to: 'sessions#create', as: :login
 end
